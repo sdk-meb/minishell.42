91aa8f95ba884e2cc9fd0500892fad039cf9f0b3
rel-hach <rel-hach@e2r2p2.1337.ma>
2022-08-16 15:33:29 +0100

    delete some unused instructions, fix some small errors, add |, unclosed quotes and quotes space then cmd errors

--- Parser/call_shell.c
+++ Parser/call_shell.c
@@ -6,11 +6,11 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 21:26:38 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/08/16 14:45:08 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/08/16 15:01:01 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../Include/minishell.h"
+#include "../Include/minishell.h"
 
 char	**ft_readline(void)
 {
@@ -25,7 +25,6 @@ char	**ft_readline(void)
 	if (!line || !line[i])
 		return (NULL);
 	add_history(line);
-	printf("%s\n", line);
 	if (!ft_check_line(line))
 	{
 		line = ft_repair_string(line);
@@ -46,14 +45,9 @@ void	ft_call_shell(void)
 		if (splitted && *splitted)
 		{
 			root = ft_create_list_for_tockens(splitted);
-			while (root != NULL)
-			{
-				printf("%s ", root->token);
-				root = root->next;
-			}
-			//root = ft_create_astree(root);
-			//print_tree(root);
 			free (splitted);
+			root = ft_create_astree(root);
+			print_tree(root);
 		}
 		splitted = ft_readline();
 	}
--- Parser/check.c
+++ Parser/check.c
@@ -6,7 +6,7 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 22:05:20 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/08/13 21:07:59 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/08/16 15:30:39 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,28 @@ bool	quotes_are_closed(char *line)
 	return (SUCCESS);
 }
 
+int	ft_failure(int error)
+{
+	if (error == 1)
+		printf("minishell: parse error near `|'\n");
+	if (error == 2)
+		printf("minishell: command not found\n");
+	if (error == 3)
+		printf("qoutes not closed\n");
+	return (FAILURE);
+}
+
 int	ft_check_line(char *line)
 {
 	int	i;
 
 	i = 0;
 	line = ft_strtrim(line, " \t\v");
-	if (line[0] == '|' || line[ft_strlen(line - 1)] == '|')
-		return (FAILURE);
+	if (line[0] == '|' || line[ft_strlen(line) - 1] == '|')
+		return (ft_failure(1));
+	if (ft_is_quote(line[0]) && line[1] == ' ')
+		return (ft_failure(2));
 	if (quotes_are_closed(line))
-		return (FAILURE);
+		return (ft_failure(3));
 	return (SUCCESS);
 }
