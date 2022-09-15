54f402fb78adf19e2f059623ddee40fb93a7c3e8
sdk-meb <use2ess@yahoo.com>
2022-09-15 14:38:40 +0100

    m2

--- Parser/pop_spcs.c
+++ Parser/pop_spcs.c
@@ -3,27 +3,33 @@
 t_list *pop_spcs(t_list *root)
 {
 	t_list *mngr;
-	int i, start = 0, end;
+	char quot = 0;
+	int i, start, end;
 	char **new;
 
 	mngr = root;
 	while (mngr)
 	{
 		i = -1;
-		while (mngr->type[++i])
+		start = -1
+		while (mngr->type[++i] && mngr->token[++start])
 		{
-			if (ft_is_quote(mngr->type[i]))
-				i = ft_get_next_quote(i, mngr->type);
-			if (ft_is_quote(mngr->type[i]) && mngr->type[i + 1] == '$')
-				i++;
-			if (mngr->type[i] == '$')
+			if (ft_is_quot(mngr->type[i]) && quot == 0)
 			{
-				start += (i++ - start);
+				quot == mngr->type[i];
+				i++;
+			}
+			else if (quot == mngr->type[i])
+			{
+				quot == 0;
+				i++;
+			}
+			if (quot == 0 && mngr->type[i] == '$')
+			{
 				end = ft_strlen(ft_get_env(mngr->type, &i)) + start--;
 				while (++start < end)
 					if (mngr->token[start] == ' ' && printf("%c",mngr->token[start]))
 						mngr->token[start] = '`';
-				printf("\n");
 			}
 		}
 		new = ft_split(mngr->token, '`');
